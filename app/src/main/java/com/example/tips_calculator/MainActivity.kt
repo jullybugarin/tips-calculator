2df738c8bbcee864dbe1ef73b5ac890799d359b5
jullybugarin <jullybugarin@gmail.com>
2024-09-07 16:29:04 +0200

    Calculations

--- app/src/main/java/com/example/tips_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tips_calculator/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tips_calculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,12 +27,51 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        binding.btnClear.setOnClickListener {
-            println("hello" + binding.tieTotal.text)
-            println("hello" + binding.tiePeople.text)
+        var percentage = 0
+        binding.btnOp1.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked)
+                percentage = 10
+        }
+        binding.btnOp2.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked)
+                percentage = 15
+        }
+        binding.btnOp3.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked)
+                percentage = 20
         }
 
 
+        binding.btnCalculate.setOnClickListener {
+            val totalTableTemp = binding.tieTotal.text
+            val totalPeopleTemp = binding.tiePeople.text
+            if (totalPeopleTemp?.isEmpty() == true || totalPeopleTemp?.isEmpty() == true
+            ) {
+                Snackbar
+                    .make(binding.tieTotal, "Fill in all empty fields", Snackbar.LENGTH_LONG)
+                    .show()
 
+            } else {
+
+                val totalPrice: Float = binding.tieTotal.text.toString().toFloat()
+                val totalPeople: Int = binding.tiePeople.text.toString().toInt()
+
+                val totalTemp = totalPrice / totalPeople
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
+                binding.tvResult.text = "Total with tips: $totalWithTips"
+                println("Total is:" + totalWithTips)
+            }
+        }
+
+
+        binding.btnClear.setOnClickListener {
+            binding.tieTotal.setText("")
+            binding.tvResult.text = ""
+            binding.tiePeople.setText("")
+            binding.btnOp1.isChecked = false
+            binding.btnOp2.isChecked = false
+            binding.btnOp3.isChecked = false
+        }
     }
 }
